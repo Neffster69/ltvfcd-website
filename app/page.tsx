5b47549f2e61b8fa0eff3d60a2eedcf841b4f9d0
Luke Michael Neff <Neffluke65@gmail.com>
2026-02-24 00:27:40 -0500

    Remove emblem image files from public

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
             <p className="text-gray-600 text-xl">Image Placeholder</p>
           </div>
         </section>
-        
+
       </main>
 
       {/* Footer */}
