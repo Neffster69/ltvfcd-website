9f63035cbf74a0d9769c5965cb98f0587ac6567e
Luke Michael Neff <Neffluke65@gmail.com>
2026-02-24 00:23:15 -0500

    Remove centered emblem section

--- app/page.tsx
+++ app/page.tsx
@@ -92,12 +92,7 @@ export default function Home() {
             <p className="text-gray-600 text-xl">Image Placeholder</p>
           </div>
         </section>
-        {/* Badge Image - Centered under Community First */}
-        <section className="w-full py-8 bg-white">
-          <div className="max-w-4xl mx-auto px-6 flex justify-center">
-            <img src="/ltvfdemblem2.png" alt="Lafayette Township Fire Department Emblem" className="max-w-xs w-full h-auto rounded shadow-md" />
-          </div>
-        </section>
+        
       </main>
 
       {/* Footer */}
